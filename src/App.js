dd72f2ed4e98c163cba81c019af5dee153f11ab7
amberhli <167585356+amberhli@users.noreply.github.com>
2025-10-05 13:31:52 -0400

    App.js displaying NavBar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+        <NavBar />
       </header>
     </div>
   );
