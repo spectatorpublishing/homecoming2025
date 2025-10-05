b35730aa5209f6e1b4dd1eac3848bd949103ca2a
aishani1604 <aishani1604@gmail.com>
2025-10-05 13:33:01 -0400

    staff credits

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import "./pages/StaffCredits.css";  
+import StaffCredits from "./pages/StaffCredits";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-
-      </header>
+      <StaffCredits />
     </div>
   );
 }
-
-export default App;
