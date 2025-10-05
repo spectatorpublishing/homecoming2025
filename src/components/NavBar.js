6b2ef9e8a15c0048a23a2ded3584b5923fed686d
amberhli <167585356+amberhli@users.noreply.github.com>
2025-10-05 13:30:41 -0400

    added fball player icons to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -10,21 +10,31 @@ const Wrapper = styled.nav`
 `;
 
 const Button = styled.button`
-  padding: 0.5rem 1rem;
+  padding: 0.7rem 1.5rem;
   border-radius: 4px;
-  font-weight: 600;
   cursor: pointer;
   transition: all 0.2s ease-in-out;
   border: none;
 
-  color: ${(props) => (props.primary ? "white" : "#2563eb")};
-  background-color: ${(props) => (props.primary ? "#2563eb" : "white")};
+  font-family: 'Outfit', sans-serif;
+  font-weight: ${(props) => (props.primary ? "600" : "400")};
+  color: ${(props) => (props.primary ? "#FFFFFF" : "#214ECF")};
+  background-color: ${(props) => (props.primary ? "#214ECF" : "#FFFFFF")};
   box-shadow: 0 2px 6px rgba(0, 0, 0, 0.1);
 
   &:hover {
     background-color: ${(props) =>
-      props.primary ? "#1e40af" : "#f0f0f0"}; /* darker blue or light hover */
+      props.primary ? "#0e3ab3ff" : "#f0f0f0"};
   }
+`;
+
+const InsideIcon = styled.img`
+  width: 80px;
+`;
+
+const OutsideIcon = styled.img`
+  width: 75px;
+  margin-top: 20px;
 `;
 
 const NavBar = () => {
@@ -37,6 +47,8 @@ const NavBar = () => {
 
   return (
     <Wrapper>
+      <OutsideIcon src="/fball-player.svg" alt="football icon" />
+      <InsideIcon src="/fball-player.svg" alt="football icon" />
       <Button primary onClick={() => handleScroll("home")}>
         Home
       </Button>
@@ -44,6 +56,8 @@ const NavBar = () => {
       <Button onClick={() => handleScroll("section2")}>Section</Button>
       <Button onClick={() => handleScroll("section3")}>Section</Button>
       <Button onClick={() => handleScroll("section4")}>Section</Button>
+      <InsideIcon src="/fball-player.svg" alt="football icon" />
+      <OutsideIcon src="/fball-player.svg" alt="football icon" />
     </Wrapper>
   );
 };
