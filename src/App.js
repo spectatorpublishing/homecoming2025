1b3f96de422b6308d5f70bfca9b02d12ad8b41f4
dmg2208 <dmg2208@columbia.edu>
2025-10-13 21:06:31 -0400

    header

--- src/App.js
+++ src/App.js
@@ -7,11 +7,13 @@ import './App.css';
 import NavBar from "./components/NavBar";
 import "./pages/StaffCredits.css";  
 import StaffCredits from "./pages/StaffCredits";
+import Letter from "./components/Letter";
 
 export default function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Letter />
         <NavBar />
       </header>
       <Section />
